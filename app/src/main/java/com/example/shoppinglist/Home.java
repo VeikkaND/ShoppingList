a40188d78d001bbf258a57613b1ed1509e79d1f7
Veikka <veikka.nevala@outlook.com>
2023-07-19 14:43:09 +0300

    navigation to creating new lists

--- app/src/main/java/com/example/shoppinglist/Home.java
+++ app/src/main/java/com/example/shoppinglist/Home.java
@@ -9,20 +9,32 @@ import android.widget.Button;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.fragment.NavHostFragment;
 
 public class Home extends Fragment {
-
     Button newButton;
 
     public Home() {
         super(R.layout.fragment_home);
     }
 
+    @Nullable
     @Override
-    public void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
 
-        newButton = getActivity().findViewById(R.id.newButton);
+        newButton = view.findViewById(R.id.newButton);
+
+        newButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                NavController navController = Navigation.findNavController(view);
+                navController.navigate(R.id.newList);
+            }
+        });
+
+        return view;
     }
-
 }
--- app/src/main/java/com/example/shoppinglist/newList.java
+++ app/src/main/java/com/example/shoppinglist/newList.java
@@ -1,13 +1,42 @@
 package com.example.shoppinglist;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 
 public class newList extends Fragment {
 
+    Button newItemButton;
+    Button backButton;
+
     public newList() {
-        // Required empty public constructor
+        super(R.layout.fragment_new_list);
     }
 
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_new_list, container, false);
+
+        newItemButton = view.findViewById(R.id.newItemButton);
+        backButton = view.findViewById(R.id.backButton);
+
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                NavController navController = Navigation.findNavController(view);
+                navController.navigate(R.id.home2);
+            }
+        });
+
+        return view;
+    }
 }
