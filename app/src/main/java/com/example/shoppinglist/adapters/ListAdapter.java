62ce89aa50bfbf42d7a1bb4b8f949cae797cfcee
Veikka <veikka.nevala@outlook.com>
2023-07-21 19:05:44 +0300

    progressbar updated with checkboxes

--- app/src/main/java/com/example/shoppinglist/adapters/ListAdapter.java
+++ app/src/main/java/com/example/shoppinglist/adapters/ListAdapter.java
@@ -1,9 +1,11 @@
 package com.example.shoppinglist.adapters;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.navigation.NavController;
@@ -13,13 +15,15 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.example.shoppinglist.R;
 import com.example.shoppinglist.entities.List;
 
+import java.util.Arrays;
+
 public class ListAdapter extends RecyclerView.Adapter<ListAdapter.ViewHolder> {
     private List[] localDataset;
 
     View.OnClickListener ListClickListener = new View.OnClickListener() {
         @Override
         public void onClick(View view) {
-            String listName = (String) view.getTag();
+            String listName = (String) view.getTag(R.id.textview);
 
             Bundle bundle = new Bundle();
             bundle.putString("listName", listName);
@@ -28,6 +32,7 @@ public class ListAdapter extends RecyclerView.Adapter<ListAdapter.ViewHolder> {
             navController.navigate(R.id.newList, bundle);
         }
     };
+
     public static class ViewHolder extends RecyclerView.ViewHolder {
         private final TextView tvListname;
 
@@ -54,7 +59,7 @@ public class ListAdapter extends RecyclerView.Adapter<ListAdapter.ViewHolder> {
 
     @Override
     public void onBindViewHolder(ViewHolder viewHolder, final int position) {
-        viewHolder.itemView.setTag(localDataset[position].listName);
+        viewHolder.itemView.setTag(R.id.textview, localDataset[position].listName);
         viewHolder.itemView.setOnClickListener(ListClickListener);
         viewHolder.getTvListname().setText(localDataset[position].listName);
     }
